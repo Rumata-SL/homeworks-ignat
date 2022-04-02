45f8e34897e11ed275fb18109e63907b2de86ab7
Rumata-SL <kirr7619@gmail.com>
2022-04-02 22:02:11 +0500

    Added styles

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,19 +1,28 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
+import h from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
+        <div className={h.wrapperHeader}>
+            {/*<div className={h.containerButton}>*/}
+            {/*    <button>hover my</button>*/}
+            {/*</div>*/}
             <div>
-                <button>+</button>
-            </div>
-
-            <ul>
-                <li><NavLink to={"/pre-junior"}>PreJunior</NavLink></li>
-                <li><NavLink to={"/junior"}>Junior</NavLink></li>
-                <li><NavLink to={"/junior-plus"}>JuniorPlus</NavLink></li>
+            <ul className={h.menu}>
+                <li className={h.current}><a href="#">nav</a></li>
             </ul>
 
+            </div>
+
+            <div className={h.containerNav}>
+
+                <ul className={h.menu}>
+                    <li><NavLink className={h.current} to={"/pre-junior"}>Pre_Junior</NavLink></li>
+                    <li><NavLink to={"/junior"}>Junior</NavLink></li>
+                    <li><NavLink to={"/junior-plus"}>Junior_Plus</NavLink></li>
+                </ul>
+            </div>
 
             {/*// add NavLinks*/}
         </div>
