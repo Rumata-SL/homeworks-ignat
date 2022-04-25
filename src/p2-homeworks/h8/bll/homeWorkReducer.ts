c064f30f2f32b633f027df392da37e30b0cdc4d8
Rumata-SL <kirr7619@gmail.com>
2022-04-25 17:28:42 +0500

    HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,24 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = {
+    type: "sort" | "check"
+    payload: string | number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
+        case "sort": {
             // need to fix
-            return state
+
+            return [...state].sort((a, b) => {
+                return action.payload === "up" ? b.age - a.age : a.age - b.age
+            })
         }
-        case 'check': {
+        case "check": {
             // need to fix
-            return state
+            return state.filter(el=> el.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
