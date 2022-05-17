80c894e4ea6f76dbdaa2e7b085e003ef3730f32c
Rumata-SL <kirr7619@gmail.com>
2022-05-18 01:02:13 +0500

    HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,31 +1,84 @@
+import Slider from 'rc-slider/lib/Slider'
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import 'rc-slider/assets/index.css';
+import {Range} from "rc-slider";
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onSuperRangeChange = (value: number) => {
+        const valueIn = Math.min(Number(value), value2);
+        setValue1(valueIn)
+    }
+    const onSuperDoubleRangeChange = (value: number[]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
     return (
-        <div>
+        <div className={'wrapper'}>
+            <h2>homework 11</h2>
             <hr/>
-            homeworks 11
+
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
+            <span className={'rangeValue'} style={{marginRight:'30px',boxSizing:'border-box', width:'50px'}}>{value1}</span>
+            <SuperRange value={value1}
+                        onChangeRange={onSuperRangeChange}
+            />
+            <span className={'rangeValue'} style={{marginLeft:'30px',boxSizing:'border-box', width:'50px'}}>{value2}</span>
+            {/*<div className={'rangeSliderBox'} style={{width:'30%', margin:'0 auto'}}>
+                <span className={'rangeValue'}>{value1}</span>
+                <Slider className={'doubleRange'}
+                        value={value1}
+                        onChange={onSuperRangeChange}
+                        railStyle={{backgroundColor: 'blue'}}
+                        handleStyle={{borderColor: 'blueviolet', backgroundColor: 'blue'}}
+                        trackStyle ={{backgroundColor: 'blueviolet'}}
                 />
+            <span className={'rangeValue'}>{value2}</span>
+
+            </div>*/}
+
+
+            <div style={{display:'flex',flexDirection:'row', justifyContent:'center',boxSizing:'border-box'}}>
+                <div className={"rangeSliderBox"}
+                     style={{
+                         width: "400px",
+                         display: "flex",
+                         flexDirection: "row",
+                         justifyContent: "center",
+                         alignItems: "center",
+                         boxSizing:'border-box'
+                     }}>
+                    <div className={"rangeValue"} style={{marginRight:'30px',boxSizing:'border-box', width:'50px'}}>{value1}</div>
+                    <Range value={[value1, value2]}
+                           className={"doubleRange"}
+                           railStyle={{
+                               backgroundColor: "blue",
+                               borderColor: "blue",
+                           }}
+                           handleStyle={[{
+                               borderColor: "blue",
+                               backgroundColor: "blue"
+                           }, {
+                               borderColor: "blue",
+                               backgroundColor: "blue"
+                           }]}
+                           trackStyle={[{
+                               backgroundColor: "white",
+                               borderColor: "white",
+
+                           }]}
+                           allowCross={false}
+                           onChange={onSuperDoubleRangeChange}/>
+                    <span className={"rangeValue"} style={{marginLeft:'30px',boxSizing:'border-box', width:'50px'}}>{value2}</span>
+                </div>
             </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -3,7 +3,8 @@ import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
 import HW9 from "../../h9/HW9";
 import HW10 from "../../h10/HW10";
-import HW111 from "../../h11/HW111";
+import HW11 from "../../h11/HW11";
+
 
 const Junior = () => {
     return (
@@ -13,7 +14,7 @@ const Junior = () => {
             <HW8/>
             <HW9/>
             <HW10/>
-            <HW111/>
+            <HW11/>
         </div>
     );
 };
