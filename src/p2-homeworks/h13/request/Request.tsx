9967664087d3c7cc29a16de84814290e1853b978
Rumata-SL <kirr7619@gmail.com>
2022-05-31 15:37:15 +0500

    HW13 fixed bug

--- src/p2-homeworks/h13/request/Request.tsx
+++ src/p2-homeworks/h13/request/Request.tsx
@@ -38,7 +38,6 @@ export const Request = () => {
     }
     return (
         <div>
-            {/*{loading && <Preloader/>}*/}
             {
                 loading
                     ? (<Preloader/>)
